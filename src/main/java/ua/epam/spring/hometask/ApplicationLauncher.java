c31dc952109d34709ffc99aba84f7551af5bbe83
Alex Baggins <Aliaksei_Miashkou@epam.com>
2017-06-18 11:50:43 +0300

    Added main() class

--- src/main/java/ua/epam/spring/hometask/ApplicationLauncher.java
+++ src/main/java/ua/epam/spring/hometask/ApplicationLauncher.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * Created by Aliaksei_Miashkou on 6/13/2017.
  */
-public class ApplicationRunner {
+public class ApplicationLauncher {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("application-config.xml");
